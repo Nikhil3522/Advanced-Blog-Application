d1a87290e7fb50b4d8fbd1d41a2d53ef5b45183f
Nikhil3522 <nikhiljha3522@gmail.com>
2023-04-04 06:57:21 +0530

    Show session storage blog to home page

--- src/Components/ShowBlog.js
+++ src/Components/ShowBlog.js
@@ -1,10 +1,53 @@
+import { useEffect, useState } from 'react';
 import { Blog, user } from '../Constants/Constants';
 
 const ShowBlog = () => {
+    const [sessionBlogList, setSessionBlogList] = useState(null);
+
+    useEffect(() => {
+        var temp = sessionStorage.getItem('blog');
+        temp = JSON.parse(temp);
+        if(temp){
+            temp.map((item, i) => {
+                const parsedObject = JSON.parse(item);
+                temp[i] = parsedObject;
+            })
+        }
+        // console.log("temp", temp)
+        setSessionBlogList(temp);
+    }, [])
     
    return (
     // <div className=" bg-pink-300">
         <div className=" flex flex-col w-[700px] m-[auto]">
+            {sessionBlogList ?
+            sessionBlogList.map(({title, content, thumbnail, author, index}) => (
+                    <div key={index+4} className="flex flex-col mt-20  border-2 p-4 bg-slate-100 shadow-md rounded-xl hover:shadow-lg hover:cursor-pointer">
+                        <div className='flex justify-around'>
+                            {user.map(({userId, userName, avatar}) => (
+                                (userId == author) ? 
+                                    <div className='w-[15%] flex flex-col'>
+                                        <img src={avatar} className='rounded-[50%] w-[70px] h-[70px] object-cover border-2 m-[auto] bg-blue-500' />
+                                        <h3 className='m-[auto]'>{userName}</h3>
+                                    </div> : null
+                            ))}
+                            <h1 className='font-bold w-[85%] text-[35px] text-center my-2 text-blue-600'>{title}</h1>
+                        </div>
+                        <div className=' flex max-h-[260px] overflow-hidden text-clip mt-4'>
+                            <div className='w-[50%]'>
+                                {console.log("thumbnail", thumbnail)}
+                                <img src={thumbnail} />
+                            </div>
+                            <div className='w-[50%]'>
+                            <p className='text-[20px] text-center mt-4 text-ellipsis'>{content}</p>
+                            </div>
+                        </div>
+                    </div>
+                )) 
+             : null}
+         
+
+
             {Blog.map(({blogId, title, content, image, author}) => (
                 <div key={blogId} className="flex flex-col mt-20  border-2 p-4 bg-slate-100 shadow-md rounded-xl hover:shadow-lg hover:cursor-pointer">
                     <div className='flex justify-around'>
