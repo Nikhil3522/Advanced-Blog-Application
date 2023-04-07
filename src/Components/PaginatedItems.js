b44e1d57e7a7da782775c044159944bac0a7d509
Nikhil3522 <nikhiljha3522@gmail.com>
2023-04-07 14:32:52 +0530

    Paginate Added

---
+++ src/Components/PaginatedItems.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react"
+import { Blog, user } from '../Constants/Constants';
+import { useNavigate } from 'react-router-dom';
+
+const PaginatedItems = () => {
+    const navigate = useNavigate();
+    const [currPage, setCurrPage] = useState(1);
+    const [sessionBlogList, setSessionBlogList] = useState(null);
+
+    useEffect(() => {
+        var temp = localStorage.getItem('blog');
+        temp = JSON.parse(temp);
+        if(temp){
+            temp.map((item, i) => {
+                const parsedObject = JSON.parse(item);
+                temp[i] = parsedObject;
+            })
+            temp.reverse();
+
+        }
+        // console.log("temp", temp)
+        setSessionBlogList(temp);
+    }, [])
+    return (
+        <div className=" flex flex-col xs:w-[100%] md:w-[700px] m-[auto] ">
+            {sessionBlogList != null ?
+            sessionBlogList.map((item, index) => (
+                index+1 <= currPage*3  && index+1 > (currPage-1)*3?
+                <div
+                        key={index+4} 
+                        className="flex flex-col mt-20  border-2 p-4 bg-slate-100 shadow-md rounded-xl hover:shadow-lg hover:cursor-pointer"
+                        onClick={() => {navigate(`/full-blog/${index}`)}}
+                    >
+                        <div className='flex justify-around'>
+                            {user.map(({userId, userName, avatar}) => (
+                                (userId == item.author) ? 
+                                    <div className='w-[15%] flex flex-col'>
+                                        <img src={avatar} className='rounded-[50%] md:w-[70px] md:h-[70px] xs:w-[50px] xs:h-[50px] object-cover border-2 m-[auto] bg-blue-500' />
+                                        <h3 className='m-[auto]'>{userName}</h3>
+                                    </div> : null
+                            ))}
+                            <h1 className='font-bold w-[85%] text-[35px] text-center my-2 text-blue-600'>{item.title}</h1>
+                        </div>
+                        <div className=' flex  max-h-[260px]  overflow-hidden text-clip mt-4'>
+                            <div className='w-[50%] '>
+                                {/* {console.log("thumbnail", item.thumbnail)} */}
+                                <img src={item.thumbnail} />
+                            </div>
+                            <div className='w-[50%] '>
+                            <p 
+                                className='text-[20px] text-center mt-4 text-ellipsis' 
+                                dangerouslySetInnerHTML={{ __html: item.content }}
+                            >
+                            </p>
+                            </div>
+                        </div>
+                    </div>: null
+            ))
+            : null}
+            <div>
+                <div onClick={() => setCurrPage(currPage - 1)}>
+                    Pre
+                </div>
+                <div>
+                    {currPage}
+                </div>
+                <div onClick={() => setCurrPage(currPage + 1)}>
+                    Next
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PaginatedItems;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import AddBlogButton from "../Components/AddBlogButton";
 import ShowBlog from "../Components/ShowBlog";
+import PaginatedItems from "../Components/PaginatedItems";
 
 const Home = () => {
     return (
         <div className="px-[50px] xs:px-[20px]">
             <AddBlogButton />
-            <ShowBlog />
+            {/* <ShowBlog /> */}
+            <PaginatedItems />
         </div>
     )
 }
